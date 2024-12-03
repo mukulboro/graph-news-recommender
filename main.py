ae1415df00417963f79e82299a8a45fc7bb652f1
Mukul <mukul.development@gmail.com>
2024-12-03 20:02:30 +0545

    Some little code for scraping

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == "__main__":
     url = "https://www.bbc.com/news"
     scraper = NewsScraper()
     news = scraper.get_all_news()
-    print(news)
+    # print(news)
--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 import bs4
 import json
-
+import time
 class NewsScraper:
     def __init__(self):
         pass
@@ -11,15 +11,31 @@ class NewsScraper:
             file_data = f.read()
             data = json.loads(file_data)
             return data
+    
+    def __scrape_websites(self, links):
+        page_data = dict()
+        for i, key in enumerate(links):
+            page_key = key.split("_")[0]
+            response = requests.get(links[key])
+            response.raise_for_status()
+            soup = bs4.BeautifulSoup(response.text, "html.parser")
+            page_data[f"{page_key}"] = soup
+        return page_data
+            
         
-    def __scrape_onlinekhabar(self, links):
-        print(links)
-        pass
+    def __parse_onlinekhabar(self, links):
+        webpage_data = self.__scrape_websites(links)
+        return webpage_data
 
     def get_all_news(self):
         news_data = self.__load_json_data()
-        onlinekhabar = self.__scrape_onlinekhabar(news_data["onlinekhabar"])
-        # response = requests.get(self.url)
-        # response.raise_for_status()
-        # soup = bs4.BeautifulSoup(response.text, "html.parser")
+        onlinekhabar = self.__parse_onlinekhabar(news_data["onlinekhabar"])
         return news_data
+    
+if __name__ == "__main__":
+    url = "https://www.bbc.com/news"
+    scraper = NewsScraper()
+    start = time.time()
+    news = scraper.get_all_news()
+    end = time.time()
+    print(f"Took: {end-start}s")
