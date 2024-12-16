4f4d1d319c7f4ae354d95a9aaf2c2dd734e32bf5
Mukul <mukul.development@gmail.com>
2024-12-16 22:26:15 +0545

    OnlineKhabar complete

--- scraper/base_scraper.py
+++ scraper/base_scraper.py
@@ -1,13 +1,13 @@
 import requests
 import bs4
 import json
-import pprint
+
 class NewsScraper:
-    def __init__(self):
-        pass
+    def __init__(self, json_dir="scraper/news_data.json"):
+        self.json_dir = json_dir
     
     def __load_json_data(self):
-        with open("news_data.json", "r") as f:
+        with open(self.json_dir, "r") as f:
             file_data = f.read()
             data = json.loads(file_data)
             return data
@@ -30,5 +30,7 @@ class NewsScraper:
             soup = bs4.BeautifulSoup(response.text, "html.parser")
             page_data[f"{page_key}"] = soup
         return page_data
-            
+
+if __name__ == "__main__":
+    test = NewsScraper()
     
--- scraper/onlinekhabar_scraper.py
+++ scraper/onlinekhabar_scraper.py
@@ -1,36 +1,15 @@
-from scraper import NewsScraper
+from .base_scraper import NewsScraper
 import bs4
 import requests
-import pprint
 
 class OnlinekhabarScraper(NewsScraper):
     def __init__(self):
         super().__init__()
+        self.news_data = self._NewsScraper__load_json_data()
+        self.webpages_data = self._NewsScraper__scrape_websites(self.news_data["onlinekhabar"])
 
-    def __parse_onlinekhabar(self, links):
-        """
-        Return data from onlinekhabar in format:
-        {
-            "breaking": [list]
-            "national": [list]
-            "intl": [list]
-            "finance": [list]
-            "entertainment": [list]
-            "sports": [list]
-        }
-
-        Each news item has following 
-        {
-            "title": string
-            "description": string
-            "url": link
-            "image": link
-        }
-        """
-        webpages_data = self._NewsScraper__scrape_websites(links)
-        all_news = dict()
-        # Work on getting breaking news first
-        breaking_page:bs4.BeautifulSoup = webpages_data["homepage"]
+    def __get_breaking_news(self):
+        breaking_page:bs4.BeautifulSoup = self.webpages_data["homepage"]
         breaking_containers = breaking_page.find_all("section", {"class": "ok-bises"})
         breaking_news_list = []
         item:bs4.element.Tag
@@ -53,10 +32,10 @@ class OnlinekhabarScraper(NewsScraper):
                 breaking_news_list.append(news_item)
             except BaseException as e:
                 continue
-        all_news["breaking"] = breaking_news_list
-
-        # Work on getting business news
-        business_page:bs4.BeautifulSoup = webpages_data["finance"]
+        return breaking_news_list
+    
+    def __get_business_news(self):
+        business_page:bs4.BeautifulSoup = self.webpages_data["finance"]
         # Have to scrape multiple containers to get info
         business_containers = business_page.find("div", {"id":"featured-carousel"}).find_all("div", {"class":"ok-news-post"})
         business_containers = [x.find("a") for x in business_containers]
@@ -83,10 +62,10 @@ class OnlinekhabarScraper(NewsScraper):
                 business_news_list.append(news_item)
             except BaseException as e:
                 continue
-        all_news["finance"] = business_news_list
-
-        # Working on entertainment news
-        entertainment_page:bs4.BeautifulSoup = webpages_data["entertainment"]
+        return business_news_list
+    
+    def __get_entertainment_news(self):
+        entertainment_page:bs4.BeautifulSoup = self.webpages_data["entertainment"]
         entertainment_containers = entertainment_page.find("div", {"class":"ok-col-left"}).find_all("div", {"class":"ok-news-post"})[:10]
         entertainment_containers = [x.find("a") for x in entertainment_containers]
         item:bs4.element.Tag
@@ -100,7 +79,7 @@ class OnlinekhabarScraper(NewsScraper):
                 news_soup = bs4.BeautifulSoup(response.text, "html.parser")
                 featured_image = ((news_soup.find("div", {"class":"post-thumbnail"})).find("img").attrs["src"]).strip()
                 top_paragraph = (news_soup.find("div", {"class":"ok18-single-post-content-wrap"}).find("p").get_text()).strip()
-                headline = news_soup.find("h1", {"class":"entry-title"}).get_text().strip()
+                headline = news_soup.find("h1").get_text().strip()
                 news_item["description"] = top_paragraph
                 news_item["title"] = headline
                 news_item["url"] = headline_link
@@ -108,10 +87,10 @@ class OnlinekhabarScraper(NewsScraper):
                 entertainment_links.append(news_item)
             except BaseException as e:
                 continue
-        all_news["entertainment"] = entertainment_links
-
-        # Working on international news
-        intl_page:bs4.BeautifulSoup = webpages_data["intl"]
+        return entertainment_links
+    
+    def __get_intl_news(self):
+        intl_page:bs4.BeautifulSoup = self.webpages_data["intl"]
         intl_containers = intl_page.find("div", {"class":"ok-col-left"}).find_all("div", {"class":"ok-news-post"})[:10]
         intl_containers = [x.find("a") for x in intl_containers]
         item:bs4.element.Tag
@@ -125,7 +104,7 @@ class OnlinekhabarScraper(NewsScraper):
                 news_soup = bs4.BeautifulSoup(response.text, "html.parser")
                 featured_image = ((news_soup.find("div", {"class":"post-thumbnail"})).find("img").attrs["src"]).strip()
                 top_paragraph = (news_soup.find("div", {"class":"ok18-single-post-content-wrap"}).find("p").get_text()).strip()
-                headline = news_soup.find("h1", {"class":"entry-title"}).get_text().strip()
+                headline = news_soup.find("h1").get_text().strip()
                 news_item["description"] = top_paragraph
                 news_item["title"] = headline
                 news_item["url"] = headline_link
@@ -133,10 +112,10 @@ class OnlinekhabarScraper(NewsScraper):
                 intl_links.append(news_item)
             except BaseException as e:
                 continue
-        all_news["intl"] = intl_links
-
-        # Working on national news
-        national_page:bs4.BeautifulSoup = webpages_data["national"]
+        return intl_links
+    
+    def __get_national_news(self):
+        national_page:bs4.BeautifulSoup = self.webpages_data["national"]
         national_containers = national_page.find("div", {"class":"ok-col-left"}).find_all("div", {"class":"ok-news-post"})[:10]
         national_containers = [x.find("a") for x in national_containers]
         item:bs4.element.Tag
@@ -148,7 +127,7 @@ class OnlinekhabarScraper(NewsScraper):
                 response = requests.get(headline_link)
                 response.encoding = "utf-8"
                 news_soup = bs4.BeautifulSoup(response.text, "html.parser")
-                headline = news_soup.find("h1", {"class":"entry-title"}).get_text()
+                headline = news_soup.find("h1").get_text().strip()
                 featured_image = ((news_soup.find("div", {"class":"post-thumbnail"})).find("img").attrs["src"]).strip()
                 top_paragraph = (news_soup.find("div", {"class":"ok18-single-post-content-wrap"}).find("p").get_text()).strip()
                 news_item["description"] = top_paragraph
@@ -158,20 +137,59 @@ class OnlinekhabarScraper(NewsScraper):
                 national_links.append(news_item)
             except BaseException as e:
                 continue
-        all_news["national"] = national_links
-
-        # Work on Sports News
-
-        # okv4-post-content
-
-        return all_news
+        return national_links
     
+    def __get_sports_news(self):
+        sports_page:bs4.BeautifulSoup = self.webpages_data["sports"]
+        sport_containers = sports_page.find_all("div", {"class":"okv4-post-content"})[:10]
+        sport_containers = [x.find("h2").find("a") for x in sport_containers]
+        item:bs4.element.Tag
+        sport_links = []
+        for item in sport_containers:
+            try:
+                news_item = dict()
+                headline_link = item.attrs["href"]
+                response = requests.get(headline_link)
+                response.encoding = "utf-8"
+                news_soup = bs4.BeautifulSoup(response.text, "html.parser")
+                headline = news_soup.find("h1").get_text().strip()
+                featured_image = ((news_soup.find("div", {"class":"post-thumbnail"})).find("img").attrs["src"]).strip()
+                top_paragraph = (news_soup.find("div", {"class":"ok18-single-post-content-wrap"}).find("p").get_text()).strip()
+                news_item["description"] = top_paragraph
+                news_item["title"] = headline
+                news_item["url"] = headline_link
+                news_item["image"] = featured_image
+                sport_links.append(news_item)
+            except BaseException as e:
+                continue
+        return sport_links
+
+
     def get_all_news(self):
-        news_data = self._NewsScraper__load_json_data()
-        onlinekhabar = self.__parse_onlinekhabar(news_data["onlinekhabar"])
-        return onlinekhabar
-    
-if __name__ == "__main__":
-    scraper = OnlinekhabarScraper()
-    news = scraper.get_all_news()
-    pprint.pprint(news)
+        """
+        Return data from onlinekhabar in format:
+        {
+            "breaking": [list]
+            "national": [list]
+            "intl": [list]
+            "finance": [list]
+            "entertainment": [list]
+            "sports": [list]
+        }
+
+        Each news item has following 
+        {
+            "title": string
+            "description": string
+            "url": link
+            "image": link
+        }
+        """
+        return {
+            "breaking": self.__get_breaking_news(),
+            "national": self.__get_national_news(),
+            "intl": self.__get_intl_news(),
+            "finance": self.__get_business_news(),
+            "entertainment": self.__get_entertainment_news(),
+            "sports": self.__get_sports_news()
+        }
